fa34b5c4e654fc7aa1c8f0350c7a73bb285586a6
有働龍太 <ryutaudo@gmail.com>
2021-12-30 18:05:55 +0900

    Solve ABC131 A Security

---
+++ src/abc131-a-security.rs
@@ -0,0 +1,14 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+  input! {
+    s: Chars,
+  }
+  for i in 0..3 {
+    if s[i] == s[i + 1] {
+      println!("Bad");
+      return;
+    }
+  }
+  println!("Good");
+}
